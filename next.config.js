060c6acbb73d79edc9b570eed01b52733ad1289b
sebastian0777 <sebastianfracgu@gmail.com>
2026-04-30 01:08:21 -0500

    Configurar export estatico y deploy automatico a GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: "/SoyMattMusic",
+  assetPrefix: "/SoyMattMusic/",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" }
     ]
